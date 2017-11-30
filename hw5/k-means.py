f46f16b16b064d2f32ba40c52816d29685db089e
SunnerLi <a6214123@gmail.com>
2017-11-30 17:48:57 +0800

    add k means

---
+++ hw5/k-means.py
@@ -0,0 +1,69 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+
+def generateData(num_points=100):
+    data = np.concatenate((np.random.normal(loc=1.0, scale=1.0, size=[num_points, 2]) , np.random.normal(loc=10.0, scale=1.0, size=[num_points, 2])))
+    plt.plot(data[:, 0], data[:, 1], 'o')
+    # plt.show()
+    return data
+
+def equal(arr1, arr2):
+    len1, len2 = np.shape(arr1)
+    for i in range(len1):
+        for j in range(len2):
+            if arr1[i][j] != arr2[i][j]:
+                return False
+    return True
+
+def K_Means(data_arr, k):
+    """
+        data_arr = (2 * N)
+    """
+    data_arr = data_arr.T
+    num_points = np.shape(data_arr)[1]
+
+    # Random Define the center
+    idx = np.asarray(range(num_points))
+    random.shuffle(idx)
+    centers = data_arr[:, idx[:k]]
+    tags = np.zeros([num_points])        
+
+    # Clustering
+    previous_center = None
+    stop_counter = 0
+    while True:        
+        # E-step
+        tags = np.empty([num_points])
+        for i in range(num_points):
+            tags[i] = np.argmin(np.sqrt(np.sum(np.square(centers - data_arr[:, i]), axis=0)))
+
+        # M-step
+        for i in range(k):
+            sub_data = data_arr[:, tags == i]
+            new_center = np.mean(sub_data, axis=-1)
+            centers[i] = new_center
+        centers = centers.T
+        # print('new center: ', centers)
+
+        # Draw
+        plt.figure(1)
+        for i in range(k):
+            sub_data = data_arr[:, tags == i]
+            plt.plot(sub_data[0], sub_data[1], 'o', label='group_%s'%str(i))
+        if not type(centers) == type(None):
+            plt.plot(centers[0], centers[1], 'o', label='center')
+        plt.title('iter_%s'%str(stop_counter))
+        plt.legend()
+        plt.show()
+
+        # Break
+        if not type(previous_center) == type(None):
+            if equal(centers, previous_center) == True or stop_counter > 20:
+                break
+        
+        previous_center = np.copy(centers)
+        stop_counter += 1
+        
+
+K_Means(generateData(), 2)
