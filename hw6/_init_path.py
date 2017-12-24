68809a253a4ef24b34c3ec696ce074f04e7b6cf0
SunnerLi <a6214123@gmail.com>
2017-12-24 15:59:22 +0800

    add plotting support vector

--- hw6/_init_path.py
+++ hw6/_init_path.py
@@ -1,7 +1,8 @@
 import sys
 
-libsvm_path = './libsvm/python/'
-tool_path = './libsvm/tools/'
+libsvm_path = './libsvm/'
+python_path = libsvm_path + 'python/'
+tool_path = libsvm_path + 'tools/'
 
-sys.path.append(libsvm_path)
+sys.path.append(python_path)
 sys.path.append(tool_path)
--- hw6/main.py
+++ hw6/main.py
@@ -5,19 +5,55 @@ from svmutil import *
 from svm import *
 from pca import *
 import subprocess
+import os
 
 # Ref:
 # https://www.csie.ntu.edu.tw/~piaip/svm/svm_tutorial.html
 
-def doSVM(list_x, list_y, best_c, best_g):
-    print('-' * 50, doSVM.__name__, '-'*50)
-    prob = svm_problem(list_y, list_x)
-    param = svm_parameter('-s 0 -t 2 -c ' + str(best_c) + ' -g ' + str(best_g) + ' -v 5')
-    model = svm_train(prob, param)
-    print(type(model))
-    svm_save_model('svm.model', model)
+color_code = ['b', 'g', 'r', 'c', 'm']
 
-def doGrid(file_name='train.txt'):
+def doSVM(list_x, list_y, best_c, best_g, model_name = 'svm.model'):
+    print('-' * 50, doSVM.__name__, '-'*50)
+    if not os.path.exists(model_name):
+        prob = svm_problem(list_y, list_x)
+        param = svm_parameter('-s 0 -t 2 -c ' + str(best_c) + ' -g ' + str(best_g))
+        model = svm_train(prob, param)
+        print(type(model))
+        svm_save_model(model_name, model)
+    else:
+        m = svm_load_model(model_name)
+        sv = m.get_SV()
+        x_train, y_train, x_test, y_test = load_data()
+        is_sv_list = np.zeros([len(list_y)])
+        for i in range(m.get_nr_sv()):
+            row = np.zeros([784])
+            for key, feature in sv[i].items():
+                row[key] = round(feature, 5)
+
+            # Find the most approximate row
+            min_value = 1000
+            min_index = -1
+            for j in range(len(x_train)):
+                diff = np.sum(np.square(x_train[j] - row))
+                # print(diff)
+                if diff < min_value:
+                    min_value = diff
+                    min_index = j
+                    # print(min_index, min_value)
+            is_sv_list[min_index] = 1
+        return is_sv_list
+
+"""
+def doSVM(best_c, best_g, file_name = 'train.txt', model_name = 'svm.model'):
+    print('-' * 50, doSVM.__name__, '-'*50)
+    args = [libsvm_path + 'svm-train', '-s', '0', '-t', '2', '-c', str(best_c), '-g', str(best_g), file_name, model_name]
+    cmd = " ".join(args)
+    print(cmd)
+    proc = subprocess.Popen(cmd, shell=True)
+    proc.wait()
+"""
+
+def doGrid(file_name = 'train.txt'):
     print('-' * 50, doGrid.__name__, '-'*50)
     args = ['grid.py', '-log2c', '-10,10,2', '-log2g', '-10,10,2', '-v', '10', '-svmtrain', './libsvm/svm-train', file_name]
     cmd = " ".join(args)
@@ -35,7 +71,7 @@ def doSubset(input_file_name = 'train.txt', output_file_name = 'subset.out', num
 if __name__ == '__main__':
     # Get data & shuffle
     x_train, y_train, x_test, y_test = load_data()
-    shuffle(x_train, y_train)
+    # shuffle(x_train, y_train)
 
     # --------------------------------------------------------------------------------------
     # Q1: Do the SVM
@@ -46,12 +82,12 @@ if __name__ == '__main__':
     doSubset(input_file_name = 'train.txt', output_file_name = 'subset.out', num = 50)
 
     # Find best hyper-parameter
-    doGrid(file_name='subset.out')   
+    #doGrid(file_name='subset.out')   
 
     # Get parameter and train 
     best_c = float(open('param.dat', 'r').readlines()[0])
     best_g = float(open('param.dat', 'r').readlines()[1])
-    doSVM(list_x, list_y, best_c, best_g)
+    is_sv_list = doSVM(list_x, list_y, best_c, best_g)
     # --------------------------------------------------------------------------------------
     # Q2: PCA projection
     # --------------------------------------------------------------------------------------
@@ -61,13 +97,21 @@ if __name__ == '__main__':
         for j, tag in enumerate(y_train):
             if tag == i:
                 num += 1
-        z_batch = np.ndarray([num, 2])
-        counter = 0
+        z_batch = []
+        sv_batch = []
         for j in range(len(y_train)):
             if y_train[j] == i:
+                _row = []
                 for k in range(2):
-                    z_batch[counter, k] = z_train[j, k]
-                counter += 1
-        plt.plot(z_batch[:, 0], z_batch[:, 1], 'o', label='digit_'+str(i))
+                    _row.append(z_train[j, k])
+                if is_sv_list[j] == 0:
+                    z_batch.append(_row)
+                else:
+                    sv_batch.append(_row)
+
+        z_batch = np.asarray(z_batch)
+        sv_batch = np.asarray(sv_batch)
+        plt.plot(z_batch[:, 0], z_batch[:, 1], 'o', color=color_code[i], label='digit_'+str(i))
+        plt.plot(sv_batch[:, 0], sv_batch[:, 1], '^', color=color_code[i], label='digit_'+str(i))
     plt.legend()
-    # plt.show()
+    plt.show()
